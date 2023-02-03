579f2d6b02d32f2092e7b8b9945771d147936ef0
gelwa <july2195@live.com>
2023-02-03 01:10:38 +0100

    PROJECT DONE

--- play.js
+++ play.js
@@ -1,20 +1,33 @@
 //random div display
-var arrow = document.querySelector(".arrows");
+var directions = document.querySelector(".directions");
+var words = document.querySelector(".words");
+var arrows = document.querySelector(".arrows");
 var score = document.querySelector(".score");
+var end = document.querySelector (".end");
 let counter = 0;
 
 
+//function to end game after 20 seconds
+setTimeout(() => {
+    end.style.display = 'block';
+    directions.textContent = "WAITING FOR PLAYER";
+
+}, 20000);
+
+//function to open a page after clicking
 function openPage(pageUrl){
     window.open(pageUrl, "_self");
 };
 
-
+//function to show score
 function drawScore() {
     score.textContent = "SCORE: " +counter;
 }
 
+//Function to randomly display words and arrows
+//draws score whenever function is called
+function getDirections() {
 
-function getWord() {
 
     var word= [
         'UP',
@@ -25,44 +38,46 @@ function getWord() {
 
     var wordNum = Math.floor(Math.random() * word.length);
 
-    arrow.textContent = word[wordNum];
-    drawScore();
+    words.textContent = word[wordNum];
 
 
-    /*var arroww= [
-        var word= [
-            '&uarr',
-            '&darr',
-            '&rarr',
-            '&larr',
+    var arroww= [
+        "\u2190",
+        "\u2191",
+        "\u2192",
+        "\u2193",
     ];
 
-    var wordNum = Math.floor(Math.random() * word.length);
+    var wordNum = Math.floor(Math.random() * arroww.length);
+    arrows.textContent = arroww[wordNum]; 
 
-    arrow.textContent = word[wordNum]; 
-*/
+    drawScore();
+
+    end.textContent = "THE ROUND HAS ENDED. YOUR SCORE: " +counter;
 
 }
 
-getWord();
+getDirections();
 
-//key press
+
+//function to react on key press, calls getDirection() for new directions
 window.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowUp' && arrow.textContent === "UP") {
+    if (event.key === 'ArrowUp' && words.textContent === "UP") {
         counter++;
-        getWord();
+        getDirections();
     }
-    else if (event.key === 'ArrowDown' && arrow.textContent === "DOWN") {
+    else if (event.key === 'ArrowDown' && words.textContent === "DOWN") {
         counter++;
-        getWord();
+        getDirections();
     }
-    else if (event.key === 'ArrowLeft' && arrow.textContent === "LEFT") {
+    else if (event.key === 'ArrowLeft' && words.textContent === "LEFT") {
         counter++;
-        getWord();
+        getDirections();
     }
-    else if (event.key === 'ArrowRight' && arrow.textContent === "RIGHT") {
+    else if (event.key === 'ArrowRight' && words.textContent === "RIGHT") {
         counter++;
-        getWord();
+        getDirections();
     }
 });
+
 
