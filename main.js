35945d6bbef933ac3ca9206fd10a04048154a7ad
gelwa <july2195@live.com>
2023-02-03 01:07:08 +0100

    Changes: removed end game pop up, added end game text (PROJECT DoNE)

--- main.js
+++ main.js
@@ -4,39 +4,26 @@ const help_button = document.querySelector (".help-button");
 const help_text = document.querySelector ("#helptext");
 const exit = document.querySelector (".exit");
 
+//function to show help text: opens a pop up/new div when clicked
 help_button.addEventListener('click', () => {
     if(help_text.style.display === 'none') {
         help_text.style.display = 'block';
     }
 });
 
+//function to exit help box (hides div)
 exit.addEventListener('click', () => {
     if(help_text.style.display === 'block') {
         help_text.style.display = 'none';
     } 
 });
 
+
+//function to open a new page when clicked
 function openPage(pageUrl){
     window.open(pageUrl, "_self");
   }
 
 
-//play.html
 
-    
-function getWord() {
-
-    var word = [
-        'up',
-        'down',
-        'left',
-        'right',
-    ];
-
-    var wordNum = Math.floor(Math.random() * word.length);
-      
-     
-    output.textContent = word[wordNum];
-  }
-
-  getWord();
+ 
